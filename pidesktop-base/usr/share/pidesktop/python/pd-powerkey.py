ccfbd7119c1cf70f7f22eb6fceab4a7e9db38c3a
Carl J. Nobile <carl.nobile@gmail.com>
2026-03-14 15:56:15 -0400

    Trying to fix the poweron button flash.

--- pidesktop-base/usr/share/pidesktop/python/pd-powerkey.py
+++ pidesktop-base/usr/share/pidesktop/python/pd-powerkey.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # pd-powerkey.py - monitor GPIO to detect power key press from Power MCU (PCU)
 #
@@ -13,8 +13,8 @@ print("pidesktop: power button service initializing")
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(31, GPIO.OUT)    # Pi to PCU - start/stop shutdown timer
-GPIO.setup(33, GPIO.IN)     # PCU to Pi - detect power key pressed
-
+# PCU to Pi - detect power key pressed
+GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.output(31, GPIO.LOW)   # tell PCU we are alive
 GPIO.output(31, GPIO.HIGH)  # cause blink by starting shutdown timer
 time.sleep(0.5)
@@ -31,8 +31,8 @@ def powerkey_pressed(channels):
 
 # wait for power key press
 print("pidesktop: power button monitor enabled")
-GPIO.add_event_detect(33, GPIO.RISING, callback=powerkey_pressed)
-
+GPIO.add_event_detect(33, GPIO.RISING, callback=powerkey_pressed,
+                      bouncetime=200)
 
 # idle - TODO: use wait
 while True:
