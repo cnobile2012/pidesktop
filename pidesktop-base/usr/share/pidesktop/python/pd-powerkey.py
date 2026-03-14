452beb7a38afd5725c7a8040d9f416c90a3070c6
Carl J. Nobile <carl.nobile@gmail.com>
2026-03-14 19:44:53 -0400

    Updated to cleaner version.

--- pidesktop-base/usr/share/pidesktop/python/pd-powerkey.py
+++ pidesktop-base/usr/share/pidesktop/python/pd-powerkey.py
@@ -5,12 +5,11 @@
 
 import gpiod
 import time
-import os
-import sys
+import subprocess
 
 print("pidesktop: power button service initializing")
 
-chip = gpiod.Chip("gpiochip0")
+chip = gpiod.Chip("/dev/gpiochip0")
 
 POWER_LED = 6      # GPIO6  (BOARD 31)
 POWER_BUTTON = 13  # GPIO13 (BOARD 33)
@@ -19,10 +18,7 @@ led = chip.get_line(POWER_LED)
 button = chip.get_line(POWER_BUTTON)
 
 led.request(consumer="pidesktop", type=gpiod.LINE_REQ_DIR_OUT)
-button.request(
-    consumer="pidesktop",
-    type=gpiod.LINE_REQ_EV_RISING_EDGE
-)
+button.request(consumer="pidesktop", type=gpiod.LINE_REQ_EV_RISING_EDGE)
 
 # Blink to show Pi is alive
 led.set_value(0)
@@ -30,17 +26,12 @@ led.set_value(1)
 time.sleep(0.5)
 led.set_value(0)
 
-
-def shutdown():
-    print("pidesktop: power button press detected, initiating shutdown")
-    os.system("sync")
-    os.system("shutdown -h now")
-    sys.exit()
-
-
 print("pidesktop: power button monitor enabled")
 
 while True:
-    if button.event_wait(sec=10):
+    if button.event_wait():
         event = button.event_read()
-        shutdown()
+        print("pidesktop: power button press detected")
+        subprocess.run(["sync"])
+        subprocess.run(["shutdown", "-h", "now"])
+        break
